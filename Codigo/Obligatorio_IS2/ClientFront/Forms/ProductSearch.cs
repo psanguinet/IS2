9d8e669e77df2f602407c19d0e2909161a739f87
psanguinet <sanguinet@gmail.com>
2015-06-13 12:17:36 -0300

    Fin desarrollo buscar productos

--- Codigo/Obligatorio_IS2/ClientFront/Forms/ProductSearch.cs
+++ Codigo/Obligatorio_IS2/ClientFront/Forms/ProductSearch.cs
@@ -13,6 +13,8 @@ namespace ClientFront.Forms
 {
     public partial class ProductSearch : MetroFramework.Forms.MetroForm
     {
+        public List<Producto> Products = new List<Producto>();
+        private int position { get; set; }
         public ProductSearch()
         {
             InitializeComponent();
@@ -23,54 +25,26 @@ namespace ClientFront.Forms
             string searchValue = this.txtSearchProducts.Text;
             string searchOption = string.Empty;
             Producto product = null;
-            List<Producto> products = new List<Producto>();
+            Products = new List<Producto>();
 
             BusinessLogic.Data info = new BusinessLogic.Data();
             if (searchValue != string.Empty)
             {
                 if (rbtNombre.Checked)
                 {
-                    searchOption = "NOMBRE";
-                    product = info.ListarProductosOrdenados().SingleOrDefault(p => p.Nombre == searchValue);
-                    if (product == null)
-                    {
-                        DialogConfirm dialog = new DialogConfirm(String.Format("El producto de nombre {0}", searchValue, " no se encuentra."));
-                    }
-                    else
-                    {
-                        ObjectToForm(product);
-                    }
+                    ShowProductByName(searchValue, ref searchOption, ref product, info);
                 }
                 if (rbtCodigo.Checked)
                 {
-                    searchOption = "CODIGO";
-                    product = info.ListarProductosOrdenados().SingleOrDefault(p => p.Codigo == searchValue);
-                    if (product == null)
-                    {
-                        DialogConfirm dialog = new DialogConfirm(String.Format("El producto con código {0}", searchValue, " no se encuentra."));
-                    }
-                    else
-                    {
-                        ObjectToForm(product);
-                    }
+                    ShowProductByCode(searchValue, ref searchOption, ref product, info);
                 }
                 if (rbtRubro.Checked)
                 {
-                    searchOption = "RUBRO";
-                    products = info.ListarProductosOrdenados().Where(p => p.Rubro == searchValue).ToList();
-                    if (product == null)
-                    {
-                        DialogConfirm dialog = new DialogConfirm(String.Format("No se encuentran productos del rubro {0}", searchValue));
-                    }
+                    ShowProductByRubro(searchValue, ref searchOption, info);
                 }
                 if (rbtTipo.Checked)
                 {
-                    searchOption = "TIPO";
-                    products = info.ListarProductosOrdenados().Where(p => p.Tipo == searchValue).ToList();
-                    if (product == null)
-                    {
-                        DialogConfirm dialog = new DialogConfirm(String.Format("No se encuentran productos del tipo {0}", searchValue));
-                    }
+                    ShowProductByTipo(searchValue, ref searchOption, info);
                 }
             }
             else
@@ -78,6 +52,68 @@ namespace ClientFront.Forms
                 MessageBox.Show("Error: debe ingresar informacion de búsqueda");
             }
 
+        }
+
+        private void ShowProductByTipo(string searchValue, ref string searchOption, BusinessLogic.Data info)
+        {
+            searchOption = "TIPO";
+            Products = info.ListarProductosOrdenados().Where(p => p.Tipo.ToUpper() == searchValue.ToUpper()).ToList();
+            if (Products.Count == 0)
+            {
+                MessageBox.Show(String.Format("No se encuentran productos del tipo {0}", searchValue));
+            }
+            else
+            {
+                position = 0;
+                Producto product = Products.ElementAt<Producto>(position);
+                ObjectToForm(product);
+                this.btnSiguiente.Visible = true;
+            }
+        }
+
+        private void ShowProductByRubro(string searchValue, ref string searchOption, BusinessLogic.Data info)
+        {
+            searchOption = "RUBRO";
+            Products = info.ListarProductosOrdenados().Where(p => p.Rubro.ToUpper() == searchValue.ToUpper()).ToList();
+            if (Products.Count == 0)
+            {
+                MessageBox.Show(String.Format("No se encuentran productos del rubro {0}", searchValue));
+            }
+            else
+            {
+                position = 0;
+                Producto product = Products.ElementAt<Producto>(position);
+                ObjectToForm(product);
+                this.btnSiguiente.Visible = true;
+            }
+        }
+
+        private void ShowProductByCode(string searchValue, ref string searchOption, ref Producto product, BusinessLogic.Data info)
+        {
+            searchOption = "CODIGO";
+            product = info.ListarProductosOrdenados().SingleOrDefault(p => p.Codigo == searchValue);
+            if (product == null)
+            {
+                MessageBox.Show(String.Format("El producto con código {0} no se encuentra.", searchValue));
+            }
+            else
+            {
+                ObjectToForm(product);
+            }
+        }
+
+        private void ShowProductByName(string searchValue, ref string searchOption, ref Producto product, BusinessLogic.Data info)
+        {
+            searchOption = "NOMBRE";
+            product = info.ListarProductosOrdenados().SingleOrDefault(p => p.Nombre.ToUpper() == searchValue.ToUpper());
+            if (product == null)
+            {
+                MessageBox.Show(String.Format("El producto de nombre {0} no se encuentra.", searchValue));
+            }
+            else
+            {
+                ObjectToForm(product);
+            }
         }
 
         private void ObjectToForm(Producto product)
@@ -90,6 +126,20 @@ namespace ClientFront.Forms
             lblDescripcionValue.Text = product.Descripcion;
             pictureBox.Image = product.Imagen;
             panelProduct.Visible = true;
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            position++;
+            if (position < Products.Count)
+            {
+                ObjectToForm(Products.ElementAt(position));
+            }
+            else
+            {
+                position = 0;
+                ObjectToForm(Products.ElementAt(position));
+            }
         }
     }
 }
